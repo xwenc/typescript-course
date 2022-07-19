ce829f57526f6f4044858e1c4f69f06d93fa06fa
thinman918 <627020231@qq.com>
2022-07-19 12:32:10 +0200

    class and interface

--- app.js
+++ app.js
@@ -1,3 +1,14 @@
-function add(a, b) {
-    return a + b;
-}
+var Members = /** @class */ (function () {
+    function Members(name, age) {
+        this.name = name;
+        this.age = age;
+    }
+    Members.prototype.nameLength = function () {
+        return this.name.length;
+    };
+    return Members;
+}());
+var members = new Members("taro", 20);
+members.nameLength();
+var members2 = { name: "Andy Xiong", nameLength: members.nameLength };
+console.log(members2.nameLength());
--- app.ts
+++ app.ts
@@ -1,3 +1,17 @@
-function add(a:number, b:number):number {
-  return a + b;
+class Members {
+  name: string;
+  age: number;
+
+  constructor(name, age) {
+    this.name = name;
+    this.age = age;
+  }
+  nameLength() {
+    return this.name.length;
+  }
 }
+
+const members = new Members("taro", 20);
+members.nameLength()
+const members2 = { name: "Andy Xiong", nameLength: members.nameLength };
+console.log(members2.nameLength());
